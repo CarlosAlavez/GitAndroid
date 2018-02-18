5df413438a78388640775e3d143f5b5329cbe97d
CarlosAlavez <carlos.alavez.estrada@gmail.com>
2018-02-17 23:54:09 -0600

    Agregamos boton que muestra imagen

--- app/src/main/java/com/example/achar/gitandroid/MainActivity.java
+++ app/src/main/java/com/example/achar/gitandroid/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.achar.gitandroid;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
-    Button saludo, saludo2;
+    Button saludo, saludo2, bimagen;
     TextView Texto;
+    ImageView imagen;
+    private  Bitmap imagenDescargada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
         saludo = (Button)findViewById(R.id.button3);
         saludo2 = (Button)findViewById(R.id.button4);
+        bimagen = (Button)findViewById(R.id.button5);
         Texto = (TextView)findViewById(R.id.lblSaludar);
+        imagen = (ImageView)findViewById(R.id.imageView);
 
         saludo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +47,36 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "se cambio el mensaje del textview", Toast.LENGTH_SHORT ).show();
             }
         });
+
+        bimagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    new DescargarImagen().execute(new URL("https://vignette.wikia.nocookie.net/toontown/images/8/8a/Toontown_Online_Game_Screenshot_Toons.png/revision/latest?cb=20131125005304"));
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    class DescargarImagen extends AsyncTask<URL, Integer, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(URL... urls) {
+            try {
+                imagenDescargada = BitmapFactory.decodeStream(urls[0].openConnection().getInputStream());
+            }catch (Exception e){
+                Log.e("diplo", "Error");
+            }
+            return  imagenDescargada;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imagen.setImageBitmap(imagenDescargada);
+            super.onPostExecute(bitmap);
+        }
     }
 
 }
