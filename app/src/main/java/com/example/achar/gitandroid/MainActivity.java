1bc314914244568701b8247463a6386dd2e81af6
CarlosAlavez <carlos.alavez.estrada@gmail.com>
2018-02-17 21:50:38 -0600

    CodigoSaluda

--- app/src/main/java/com/example/achar/gitandroid/MainActivity.java
+++ app/src/main/java/com/example/achar/gitandroid/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.achar.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button saludo;
+    TextView Texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        saludo = (Button)findViewById(R.id.button3);
+        Texto = (TextView)findViewById(R.id.lblSaludar);
 
+        saludo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Texto.setText("Hola GitHub");
+            }
+        });
     }
+
 }
